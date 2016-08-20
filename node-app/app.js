04bcca8a058029771754d25694063b047469b291
Chris Parsons <christopherkparsons@gmail.com>
2016-08-20 15:11:52 +0100

    sending html from app.js

--- node-app/app.js
+++ node-app/app.js
@@ -8,19 +8,11 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
-  // res.writeHead(200,{'Content-Type':'text/html'});
-  // fs.createReadStream(__dirname + '/views/index.html','utf8').pipe(res);
-  res.status(200).send("hello world")
+  res.writeHead(200,{'Content-Type':'text/html'});
+  fs.createReadStream(__dirname + '/views/index.html','utf8').pipe(res);
 });
 
 var server = app.listen(process.env.PORT || '8080', function(){
   console.log('App listening on port %s', server.address().port);
   console.log('Press Ctrl+C to quit');
-});
-
-var GoogleMapsLoader = require('google-maps');
-GoogleMapsLoader.KEY = 'AIzaSyBBbIYhZtC48MceLvgJ3-dIBjgezkSeu-g';
-GoogleMapsLoader.LIBRARIES = ['geometry'];
-GoogleMapsLoader.load(function(google) {
-    new google.maps.Map(el, options);
 });
