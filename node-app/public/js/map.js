78fd14b35062366dafa707815d588325de2d2d60
Chris Parsons <christopherkparsons@gmail.com>
2016-08-20 14:06:56 +0100

    removing extra layer line

--- node-app/public/js/map.js
+++ node-app/public/js/map.js
@@ -1,19 +1,16 @@
 function intiMap(){
 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-	center: new google.maps.LatLng(30,0),
-	zoom: 2,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
+		center: new google.maps.LatLng(30,0),
+		zoom: 2,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
 
 	var world_geometry = new google.maps.FusionTablesLayer({
 		query: {
-	select: 'geometry',
-	from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk'
-	},
-	map: map,
-	suppressInfoWindows: true
+				select: 'geometry',
+				from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk'
+			},
+			map: map,
+			suppressInfoWindows: true
 	});
-  layer.setMap(map);
-
 }
-
